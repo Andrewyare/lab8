5c81527b2db8205006c397a76e3e172132968eb7
andrewyare <91269160+Andrewyare@users.noreply.github.com>
2022-12-11 15:17:02 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'super_secret_key'
+
+
+#Database
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'site.db')
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
